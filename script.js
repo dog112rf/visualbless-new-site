85be787abd78495d04526e4d069239ac14b190b5
finestwebdesign <deathkv2.0@gmail.com>
2023-03-09 16:43:14 +0300

    almost done white theme

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+// возвращает куки с указанным name,
+// или undefined, если ничего не найдено
+
+  function setCookie(name, value, options = {}) {
+
+	options = {
+	  path: '/',
+	  // при необходимости добавьте другие значения по умолчанию
+	  ...options
+	};
+  
+	if (options.expires instanceof Date) {
+	  options.expires = options.expires.toUTCString();
+	}
+  
+	let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  
+	for (let optionKey in options) {
+	  updatedCookie += "; " + optionKey;
+	  let optionValue = options[optionKey];
+	  if (optionValue !== true) {
+		updatedCookie += "=" + optionValue;
+	  }
+	}
+  
+	document.cookie = updatedCookie;
+  }
+
+  function deleteCookie(name) {
+	setCookie(name, "", {
+	  'max-age': -1
+	})
+  }
+
+  function getCookie(name) {
+	let matches = document.cookie.match(new RegExp(
+	  "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+	));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+  }
+
 var portfolio_menu = document.getElementById("portfolio")
 var portfolio_button = document.getElementById("portfolio_button")
 
@@ -68,3 +109,61 @@ var main_slide_3 = document.getElementById("main_slide_3_href")
 main_slide_1.href="/index.html#latestnews"; 
 main_slide_2.href="/index.html#latestnews"; 
 main_slide_3.href="/index.html#latestnews"; 
+
+var date = new Date()
+
+var hours = date.getHours()
+
+var themechanger = document.getElementById("themechanger")
+
+var bodyid = document.getElementById("bodyid")
+
+console.log(hours)
+
+var theme = getCookie("theme")
+var themechangedmanually = getCookie("themechangedmanually")
+
+
+if (themechangedmanually == undefined) {
+	themechangedmanually = false
+}
+
+if (getCookie("theme") == "dark") {
+	document.body.classList.remove("light")
+	totaltheme = "dark"
+} else {
+	document.body.classList.add("light")
+	totaltheme = "light"
+}
+
+if (theme == undefined) {
+	if (hours>21 && themechangedmanually == false) {
+		var totaltheme = "dark"
+		bodyid.classList.remove("light")
+	
+	} if (hours>21 && themechangedmanually == true) {
+	
+	} if (hours<21 && themechangedmanually == false) {
+		var totaltheme = "light"
+		bodyid.classList.add("light")
+
+	} if (hours<21 && themechangedmanually == true) {
+	
+	}
+}
+
+
+themechanger.addEventListener("click",function(){
+	if (totaltheme == "dark") {
+		deleteCookie("theme")
+		bodyid.classList.add("light")
+		totaltheme = "light"
+		setCookie("theme","light")
+		setCookie("themechangedmanually", true)
+	} else {
+		bodyid.classList.remove("light")
+		setCookie("theme","dark")
+		totaltheme = "dark"
+		setCookie("themechangedmanually", true)
+	}
+})
